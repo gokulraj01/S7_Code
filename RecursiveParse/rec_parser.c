32b26ec53f9629139e0a81c435a047822ef2f3f6
rogue-geek <84265206+rogue-geek@users.noreply.github.com>
2023-11-07 09:07:09 +0530

    Recursive Parser basic implement

--- RecursiveParse/rec_parser.c
+++ RecursiveParse/rec_parser.c
@@ -20,7 +20,6 @@ void err(char *msg){
 }
 
 int findDelim(char *s, char delim, int l){
-    printf("checking '%s' [len:%d] for '%c'\n", s, l, delim);
     int i = 0;
     while(i < l){
         if(s[i] == delim) return i;
@@ -36,40 +35,53 @@ char F(char *s, int l);
 char G(char *s, int l);
 char N(char *s, int l);
 
+char shb[64];
+
+char* shown(char *s, int l){
+    strncpy(shb, s, l);
+    shb[l] = 0;
+    return shb;
+}
 
 char N(char *s, int l){
+    printf("N? -> %s\n", shown(s,l));
     for(int i=0; i<l; i++)
-        if(s[i] <= '0' || s[i] >= '9') return 0;
+        if((s[i] <= '0' || s[i] >= '9') && s[i] != ' ' && s[i] != '\n') return 0;
+    printf("Yes!!\n");
     return 1;
 }
 
 char G(char *s, int l){
+    printf("G? -> %s\n", shown(s,l));
     int i = findDelim(s,'(',l);
     int j = findDelim(s,')',l);
+    printf("( at %d ) at %d\n", i, j);
     if(i == -1 || j == -1) return N(s, l);
     else return E(&s[i+1], j-i-1);
 }
 
 char F(char *s, int l){
+    printf("F? -> %s\n", shown(s,l));
     int i = findDelim(s,'*',l);
     int j = findDelim(s,'/',l);
-    
+    printf("* at %d / at %d\n", i, j);
     char res = 0;
     res = res || G(s,l);
-    if(i > -1) res = res || F(s, i) && G(&s[i], l-i);
-    if(j > -1) res = res || F(s, j) && G(&s[j], l-j);
+    if(i > -1 && !res) res = res || F(s, i) && G(&s[i+1], l-i-1);
+    if(j > -1 && !res) res = res || F(s, j) && G(&s[j+1], l-j-1);
     
     return res;
 }
 
 char E(char *s, int l){
+    printf("E? -> %s\n", shown(s,l));
     int i = findDelim(s,'+',l);
     int j = findDelim(s,'-',l);
-    
+    printf("+ at %d - at %d\n", i, j);
     char res = 0;
     res = res || F(s,l);
-    if(i > -1) res = res || E(s, i) && F(&s[i], l-i);
-    if(j > -1) res = res || E(s, j) && F(&s[j], l-j);
+    if(i > -1 && !res) res = res || E(s, i) && F(&s[i+1], l-i-1);
+    if(j > -1 && !res) res = res || E(s, j) && F(&s[j+1], l-j-1);
     
     return res;
 }
@@ -79,7 +91,8 @@ void main(int argc, char **argv){
     while(1){
         printf(">> ");
         fgets(buff, 256, stdin);
-        int l = strlen(buff);
+        int l = strlen(buff)-1;
+        buff[l] = 0;
         if(!E(buff, l)) printf("Parse failed!!\n");
     }
 }
